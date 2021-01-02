b69af7e35c5bc673014cf2360cf23d3cb97aab8a
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 00:53:52 +0330

    Add theming system
    Created src/theme/Colors.js which includes all colors of
    the application's themes
    The only available theme is currently Light Theme

---
+++ src/theme/Colors.js
@@ -0,0 +1,12 @@
+//Colors that will be used across the application
+
+//Light theme's colors
+export const Light = {
+    primary: "#4361EE",
+    primaryInner: "#FFF",
+    secondary: "#06D6A0",
+    border: "#F1F3F5",
+    icon: "#CED4DA",
+    main: ["#FFFFFF", "#F8F9FA", "#F1F3F5"],
+    text: ["#212529", "#6C757D", "#98A0A8", "#ADB5BD"]
+};

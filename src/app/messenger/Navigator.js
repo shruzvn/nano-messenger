60cdf365e3c5c0bae6f452cae1bee25d4055516f
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 01:56:38 +0330

    Add Navigator
    Created the navigator component and its events and data

---
+++ src/app/messenger/Navigator.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+import { Main } from '../../common/Containers';
+import IconButton from '../../util/components/IconButton';
+
+const Container = styled(Main).attrs({
+    flexEnabled: true, xAlign: "center", yAlign: "center", borderDirection: "right"
+})`
+    padding: 1.5rem;
+    flex-direction: column;
+    div{
+        margin-top: 2rem;
+        &:first-of-type{
+            margin-top: 0;
+        }
+    }
+`;
+
+const Buttons = [
+    { id: "message", icon: "message" },
+    { id: "contacts", icon: "contacts" },
+    { id: "writeMessage", icon: "create", primary: true },
+    { id: "settings", icon: "settings" },
+    { id: "info", icon: "info" },
+]
+
+function Navigator({ onSelect, active }) {
+    return (
+        <Container>
+            {Buttons.map(({ id, icon, primary }) =>
+                <IconButton onClick={onSelect} active={active === id} id={id} key={id} type={primary ? 3 : 1} icon={icon} />
+            )}
+        </Container>
+    )
+}
+
+export default Navigator;

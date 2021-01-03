ca2cb6987a7ef062251a09c4a2c0fa09b3b8ab20
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 13:53:23 +0330

    Add menu to the application
    Imported Menu.js to the Messenger.js
    Updated the styles a little bit to show Menu properly

--- src/app/messenger/Messenger.js
+++ src/app/messenger/Messenger.js
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 import { CSSTransition } from 'react-transition-group';
 
 import { Main, MaxToParent } from '../../common/Containers';
+import Menu from './Menu';
 import Navigator from './Navigator';
 
-const Container = styled(Main).attrs({as: "main", flexEnabled: true})`
+const MainContainer = styled(Main).attrs({ as: "main", flexEnabled: true })`
     height: 100vh;
+    flex-direction: column;
 `;
 
-const ActivityWrapper = styled(Main)`
+const Container = styled(Main)`
     flex: 1;
 `;
 
@@ -29,23 +31,26 @@ const children = [
 function Messenger() {
     const [currentActivity, setActivity] = useState("messages");
 
-    const changeActivity = e=>{
+    const changeActivity = e => {
         setActivity(e.currentTarget.id);
     }
 
     return (
-        <Container>
-            <Navigator onSelect={changeActivity} active={currentActivity} />
-            <ActivityWrapper>
-                {children.map(child =>
-                    <CSSTransition in={child.id === currentActivity} {...animationProps}>
-                        <MaxToParent>
-                            {child.txt}
-                        </MaxToParent>
-                    </CSSTransition>
-                )}
-            </ActivityWrapper>
-        </Container>
+        <MainContainer>
+            <Menu />
+            <Container flexEnabled>
+                <Navigator onSelect={changeActivity} active={currentActivity} />
+                <Container>
+                    {children.map(child =>
+                        <CSSTransition in={child.id === currentActivity} {...animationProps}>
+                            <MaxToParent>
+                                {child.txt}
+                            </MaxToParent>
+                        </CSSTransition>
+                    )}
+                </Container>
+            </Container>
+        </MainContainer>
     )
 }
 

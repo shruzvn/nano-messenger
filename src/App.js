1e4d8a163eb67d649837142aea7ea5074549b23b
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 18:16:20 +0330

    Add redux to the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {enableMobileView} from './app/redux/actions';
 
 import { Light, Dark } from './theme/Colors';
 import Messenger from './app/messenger/Messenger';
 
 function App() {
-    const [darkMode, enableDarkMode] = useState(true);
+    const darkMode = useSelector(state => state.darkMode);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        detectMobileMode();
+        window.addEventListener("resize", detectMobileMode, false);
+        return () => {
+            window.removeEventListener("resize", detectMobileMode, false);
+        }
+        //eslint-disable-next-line
+    }, []);
+
+    const detectMobileMode = () => {
+        dispatch(enableMobileView(window.innerWidth < 1280));
+    }
+
     return (
         <ThemeProvider theme={darkMode ? Dark : Light}>
-            <Messenger darkMode={darkMode} onThemeChange={() => { enableDarkMode(!darkMode) }} />
+            <Messenger />
         </ThemeProvider>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import GlobalStyles from './theme/GlobalStyles';
 import App from './App';
+import store from './app/redux/store';
 
 ReactDOM.render(
   [
-    <GlobalStyles key={0}/>,
-    <App key={1}/>
+    <GlobalStyles key={0} />,
+    <Provider key={1} store={store}>
+      <App />
+    </Provider>
   ],
   document.getElementById('root')
 );

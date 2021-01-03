5994a097594a4d65f2fa17d27730c0ee8fb89161
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 14:49:21 +0330

    Add dark colors

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -10,3 +10,14 @@ export const Light = {
     main: ["#FFFFFF", "#F8F9FA", "#F1F3F5"],
     text: ["#212529", "#6C757D", "#98A0A8", "#ADB5BD"]
 };
+
+//Dark theme's colors
+export const Dark = {
+    primary: "#4361EE",
+    primaryInner: "#FFF",
+    secondary: "#06D6A0",
+    border: "#282E32",
+    icon: "#8D959D",
+    main: ["#212529", "#262B2F", "#343A40"],
+    text: ["#FFF", "#6C757D", "#98A0A8", "#ADB5BD"]
+};

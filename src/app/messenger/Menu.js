9fcd9babbffeda1480542c9d1e1b8c3f92edfb41
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-03 18:18:51 +0330

    Add redux support for src/app/messenger/Menu.js

--- src/app/messenger/Menu.js
+++ src/app/messenger/Menu.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {toggleDarkMode} from '../redux/actions';
 
 import { Main } from '../../common/Containers';
 import IconButton from '../../util/components/IconButton';
@@ -14,12 +17,17 @@ const RightGroup = styled(Main).attrs({ as: "div", flexEnabled: true })`
     }
 `;
 
-function Menu({darkMode, onThemeChange}) {
+function Menu() {
+    const darkMode = useSelector(state => state.darkMode);
+    const dispatch = useDispatch();
+
     return (
         <Container>
             <RightGroup>
                 <IconButton icon="power_settings_new" type={1}/>
-                <IconButton onClick={onThemeChange} icon={["light_mode", "dark_mode"]} animationState={darkMode} type={2}/>
+                <IconButton onClick={()=>{
+                    dispatch(toggleDarkMode(!darkMode))
+                }} icon={["light_mode", "dark_mode"]} animationState={darkMode} type={2}/>
             </RightGroup>
         </Container>
     )

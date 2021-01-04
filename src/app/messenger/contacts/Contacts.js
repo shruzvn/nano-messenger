d8acc2f594739a4d4f064dd26338ea82e225e369
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-04 16:48:14 +0330

    Update src/app/messenger/contacts/Contacts.js
    Created a fake contacts list by using InfoBoxe and LetterSeparator components

--- src/app/messenger/contacts/Contacts.js
+++ src/app/messenger/contacts/Contacts.js
@@ -1,14 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
 import ActivityMaker from '../../../util/components/ActivityMaker';
+import InfoBox from '../../../util/components/InfoBox';
+import Scrollbar from '../../../util/components/Scrollbar';
+import { TruncateChar } from '../../../util/Tools';
+import LetterSeparator from './LetterSeparator';
+
+const FakeData = [
+    { id: "alannaCobbet", name: "Alanna Cobbet", lastseen: "Online", img: true },
+    { id: "austinCharles", name: "Austin Charles", lastseen: "yesterday", img: false },
+    { id: "beverleyCastill", name: "Beverley Castill", lastseen: "2 Jan", img: true },
+    { id: "danielBrown", name: "Daniel Brown", lastseen: "2 mins ago", img: true },
+    { id: "jamesMegan", name: "James Megan", lastseen: "Online", img: true },
+];
+
+const getLetter = (x, y) => {
+    const letterX = TruncateChar(x, 1).toUpperCase(),
+        letterY = y ? TruncateChar(y, 1).toUpperCase() : null;
+    return letterX !== letterY ? <LetterSeparator letter={letterX} /> : null;
+}
 
 function Contacts() {
     const mobileView = useSelector(state => state.mobileView);
 
     const [activityState, setActivityState] = useState(0);
 
-    useEffect(()=>{
+    useEffect(() => {
         setActivityState(0);
     }, [mobileView])
 
@@ -23,7 +41,24 @@ function Contacts() {
     return (
         <ActivityMaker mobileMode={mobileView}
             activityState={activityState} onBack={moveToPreviousActivity}>
-            <div style={{width: "100%", height: "100%"}} onClick={moveToNextActivity}>Your Contacts List</div>
+            <Scrollbar searchbar="people">
+                {
+                    FakeData.map((data, i) =>
+                        <Fragment key={i}>
+                            {getLetter(data.name, FakeData[i - 1] && FakeData[i - 1].name)}
+                            <InfoBox onClick={moveToNextActivity} profile={{
+                                img: data.img && "img/" + data.id + ".jpg",
+                                name: data.name,
+                                online: data.lastseen === "Online"
+                            }}>
+                                <h4>{data.name}</h4>
+                                <p>{data.lastseen === "Online" ?
+                                    "Online" : "Last seen " + data.lastseen}</p>
+                            </InfoBox>
+                        </Fragment>
+                    )
+                }
+            </Scrollbar>
             <div>Your Selected Contact</div>
         </ActivityMaker>
     )

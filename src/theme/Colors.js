1d39c06971e9c952383f515cbf9d1ce790489593
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-06 02:17:42 +0330

    Add delete(error) color value

--- src/theme/Colors.js
+++ src/theme/Colors.js
@@ -7,6 +7,7 @@ export const Light = {
     secondary: "#06D6A0",
     border: "#F1F3F5",
     icon: "#CED4DA",
+    delete: "#F52A50",
     main: ["#FFFFFF", "#F8F9FA", "#F1F3F5"],
     text: ["#212529", "#6C757D", "#98A0A8", "#ADB5BD"]
 };
@@ -18,6 +19,7 @@ export const Dark = {
     secondary: "#06D6A0",
     border: "#282E32",
     icon: "#8D959D",
+    delete: "#F52A50",
     main: ["#212529", "#262B2F", "#343A40"],
     text: ["#FFF", "#6C757D", "#98A0A8", "#ADB5BD"]
 };

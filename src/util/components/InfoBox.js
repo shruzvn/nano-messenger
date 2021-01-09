4d86045cbf5be4e26a351598302f86e3838cbe7e
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-09 19:49:01 +0330

    fix(UX): Sepration between click and swip gesture

--- src/util/components/InfoBox.js
+++ src/util/components/InfoBox.js
@@ -22,8 +22,8 @@ const DeleteContainer = styled(Main).attrs({ as: "div", yAlign: "center", xAlign
     padding: 1.5rem;
     position: absolute;
     top: 50%;
-    left: 2%;
-    width: 98%;
+    left: 0;
+    width: 100%;
     height: 98%;
     transform: translateY(-50%);
     background-color: ${({ theme }) => theme.delete};
@@ -50,7 +50,9 @@ const Container = styled(Group)`
     `}
 `;
 
-function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) {
+let separateMouseDownAndTouchStarts = 0;
+
+function InfoBox({ children, profile, active, disableSwip, onDelete, onClick }) {
     const [defaultPoints, initDefualtPoints] = useState(null),
         [point, setPoint] = useState(0),
         [swipping, enableSwipping] = useState(null);
@@ -62,6 +64,7 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
                 window.addEventListener("touchmove", swip, false);
                 window.addEventListener("touchend", swipEnds, false);
                 window.addEventListener("touchcancel", swipEnds, false);
+                separateMouseDownAndTouchStarts++;
             } else {
                 window.addEventListener("mousemove", swip, false);
                 window.addEventListener("mouseup", swipEnds, false);
@@ -71,17 +74,19 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
     }, [defaultPoints]);
 
     useEffect(() => {
-        if (!swipping) {
-            if (point > 100)
-                onDelete && onDelete();
-            else
-                setPoint(0);
+        if (swipping === false) {
+            if (point === 0)
+                onClick && onClick();
+            else if (-point > 200)
+                console.log("Delete");
+            setPoint(0);
         }
         //eslint-disable-next-line
     }, [swipping]);
 
     const swipStarts = e => {
         if (!disableSwip) {
+            console.log(e.type)
             let clientX, clientY, isTouchScreen = false;
             if (e.changedTouches) {
                 clientX = e.changedTouches[0].clientX;
@@ -89,11 +94,14 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
                 isTouchScreen = true;
             }
             else {
+                if (separateMouseDownAndTouchStarts > 0) {
+                    separateMouseDownAndTouchStarts = 0;
+                    return false;
+                }
                 clientX = e.clientX;
                 clientY = e.clientY;
-            }
-            if (!isTouchScreen)
                 e.preventDefault();
+            }
             initDefualtPoints({ x: clientX, y: clientY, isTouchScreen });
         }
     };
@@ -103,6 +111,7 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
         if (e.changedTouches) {
             clientX = e.changedTouches[0].clientX;
             clientY = e.changedTouches[0].clientY;
+            separateMouseDownAndTouchStarts = 0;
         }
         else {
             clientX = e.clientX;
@@ -117,14 +126,14 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
         setPoint(calculatedPoint);
     };
 
-    const swipEnds = () => {
+    const swipEnds = e => {
         window.removeEventListener("mousemove", swip, false);
         window.removeEventListener("touchmove", swip, false);
         window.removeEventListener("mouseup", swipEnds, false);
         window.removeEventListener("touchend", swipEnds, false);
         window.removeEventListener("touchcancel", swipEnds, false);
-        enableSwipping(false);
         initDefualtPoints(null);
+        enableSwipping(false);
     };
 
     return (
@@ -135,7 +144,8 @@ function InfoBox({ children, profile, onClick, active, disableSwip, onDelete }) 
             <Container onTouchStart={swipStarts} onMouseDown={swipStarts}
                 swipEnabled={point < 0}
                 enableTransition={!swipping}
-                style={{ transform: "translateX(" + point + "px)" }} active={active} onClick={onClick}>
+                style={{ transform: "translateX(" + point + "px)" }}
+                active={active}>
                 <Profile {...profile} />
                 <div style={{ flex: 1, marginLeft: "1rem" }}>
                     <Group>

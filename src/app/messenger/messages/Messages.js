bccf552a8f3c8f6223c043073d1aaa9ee208c598
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-14 19:11:32 +0330

    refactor: Create a separate component for MessageBoxes

--- src/app/messenger/messages/Messages.js
+++ src/app/messenger/messages/Messages.js
@@ -1,27 +1,15 @@
-import styled from 'styled-components';
-
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import ActivityMaker from '../../../util/components/ActivityMaker';
-import InfoBox from '../../../util/components/InfoBox';
 import Scrollbar from '../../../util/components/Scrollbar';
-import ReadStatus from './ReadStatus';
-
-const MessageTime = styled.div`
-    font-size: .9rem;
-    color: ${({ theme }) => theme.text[1]};
-    margin: 0 0 0 auto;
-`;
-
-const From = styled(MessageTime)`
-    margin: 0 .3rem 0 0;
-`;
+import MessageBox from './MessageBox';
 
 const FakeData = [
-    { id: "danielBrown", name: "Daniel Brown", lastMsg: "Be there soon", time: "2 Mins ago" },
-    { id: "alannaCobbet", name: "Alanna Cobbet", lastMsg: "Arigato!", time: "5 Mins ago", fromYou: true, readState: false },
-    { id: "violetPierpoint", name: "Violet Pirepoint", lastMsg: "Kinda", time: "1 Hour ago", fromYou: true, readState: true, online: true },
+    { id: "danielBrown", name: "Daniel Brown", message: "Be there soon", time: "2 Mins ago", unread: 1 },
+    { id: "alannaCobbet", name: "Alanna Cobbet", message: "Arigato!", time: "5 Mins ago", from: "You", readState: false },
+    { id: "violetPierpoint", name: "Violet Pirepoint", message: "Kinda", time: "1 Hour ago", from: "You", readState: true, online: true },
+    { id: "gng", name: "Our Gang", message: "Well well well", time: "2 Hours ago", from: "Daniel Brown", isGroup: true},
 ];
 
 function Messages() {
@@ -46,18 +34,7 @@ function Messages() {
             activityState={activityState} onBack={moveToPreviousActivity}>
             <Scrollbar searchbar="messages">
                 {FakeData.map(data =>
-                    <InfoBox onClick={moveToNextActivity} key={data.id}
-                        profile={{ name: data.name, online: data.online }}>
-                        <>
-                            <h4>{data.name}</h4>
-                            <MessageTime>{data.time}</MessageTime>
-                        </>
-                        <>
-                            {data.fromYou && <From>You:</From>}
-                            <p>{data.lastMsg}</p>
-                            {data.fromYou && <ReadStatus state={data.readState} />}
-                        </>
-                    </InfoBox>
+                    <MessageBox key={data.id} onClick={moveToNextActivity} {...data}/>
                 )}
             </Scrollbar>
             <div>Your Selected Contact</div>

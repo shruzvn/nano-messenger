a34dbc02a9be4c00681aa844cab8019aee5c19d4
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-15 01:37:12 +0330

    feat: Create empty selected chat messages container

---
+++ src/app/messenger/messages/chat/MessagesArea.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    flex: 1;
+`;
+
+function MessagesArea(){
+    return (
+        <Container/>
+    )
+}
+
+export default MessagesArea;

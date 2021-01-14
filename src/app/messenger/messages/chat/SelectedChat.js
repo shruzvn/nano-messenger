076dfe1f8e5e15d0b6163e79fc466a9e9454df15
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-15 01:35:08 +0330

    feat: Create the selected chat component

---
+++ src/app/messenger/messages/chat/SelectedChat.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+import TopBar from './TopBar';
+import MessageInput from './MessageInput';
+import MessagesArea from './MessagesArea';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+`;
+
+function SelectedChat({ user, mobileMode, onBack }) {
+    return (
+        <Container>
+            {user &&
+                <>
+                    <TopBar isGroup={user.isGroup}
+                        name={user.name}
+                        backEnabled={mobileMode}
+                        onBack={onBack}
+                        online={user.lastseen === "Online"}
+                        bottom={user.isGroup ? user.members.length + " Members" : user.lastseen} />
+                    <MessagesArea />
+                    <MessageInput />
+                </>
+            }
+        </Container>
+    )
+}
+
+export default SelectedChat;

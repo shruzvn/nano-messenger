75a565f73db55754329628d7fa3b2c04be135ed0
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-15 01:35:48 +0330

    feat: Create top menu of selected chat

---
+++ src/app/messenger/messages/chat/TopBar.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import InfoBox from '../../../../util/components/InfoBox';
+import { Main as Container } from '../../../../common/Containers';
+
+const GroupIcon = styled.i.attrs({ className: "material-icons-outlined" })`
+    margin-right: .5rem;
+    font-size: 1.2rem;
+    color: ${({theme})=> theme.text[1]};
+    transition: 200ms color;
+`;
+
+function TopBar({isGroup, name, bottom, online, backEnabled, onBack}) {
+    return (
+        <Container as="div" borderDirection="down" >
+            <InfoBox backEnabled={backEnabled} onBack={onBack} profile={{ name: name, online }} disableSwip>
+                <>
+                    {isGroup && <GroupIcon>group</GroupIcon>}
+                    <h4>{name}</h4>
+                </>
+                <p>{bottom}</p>
+            </InfoBox>
+        </Container>
+    )
+}
+
+export default TopBar;

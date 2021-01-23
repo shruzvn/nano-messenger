f59510d8e97b3dd1960b4b0e03783cf759a3f4c9
Shayan Vahabnezhad <shynvn@outlook.com>
2021-01-24 02:56:04 +0330

    feat: Add messages bubble (private and group chat)

---
+++ src/app/messenger/messages/chat/MessageBubble.js
@@ -0,0 +1,95 @@
+import styled, { css } from 'styled-components';
+
+import { Main } from '../../../../common/Containers';
+import ReadStatus from './../ReadStatus';
+import Profile from '../../../../util/components/Profile'
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: flex-end;
+    figure{
+        margin: 0 1rem 2rem 0;
+        cursor: pointer;
+    }
+    margin-bottom: 1.2rem;
+    &:last-of-type{
+        margin-bottom: 0;
+    }
+`;
+
+const StatusContainer = styled(Main).attrs({ flexEnabled: true, yAlign: "center" })`
+    color: ${({ theme }) => theme.text[2]};
+    font-size: .8rem;
+    overflow: hidden;
+    padding: .6rem 0;
+    transition-property: color, opacity;
+`;
+
+export const Bubble = styled(Main).attrs({ as: "div" })`
+    padding: 1rem 1.2rem;
+    background-color: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.primaryInner};
+    border-radius: 1.5rem;
+    font-size: .9rem;
+    margin-top: .5rem;
+    max-width: 100%;
+    min-width: 10rem;
+    width: fit-content;
+    &:first-of-type{
+        margin-top: 0;
+    }
+`;
+
+const Container = styled.div`
+    max-width: 80%;
+    ${({ type }) => type === 1 ? 
+        css`
+            margin-left: auto;
+            ${Bubble}:last-of-type{
+                border-radius: 1.5rem 1.5rem .5rem 1.5rem;
+            }
+        ` : css`
+            margin-right: auto;
+            ${Bubble}{
+                background-color: ${({ theme }) => theme.main[1]};
+                color: ${({ theme }) => theme.text[0]};
+                ${type !== 2 ? css`
+                    &:first-of-type{
+                        border-radius: .5rem 1.5rem 1.5rem 1.5rem;
+                    }`
+                    : css`
+                    :last-of-type{
+                        border-radius: 1.5rem 1.5rem 1.5rem .5rem;
+                    }`
+                }
+            }
+        `
+    }
+    p{
+        margin-bottom: .8rem;
+        margin-left: .4rem;
+        color: ${({theme})=> theme.text[2]};
+        transition: 200ms color;
+        font-size: .9rem;
+        font-weight: bold;
+    }
+`;
+
+function MessageContainer(props) {
+    return (
+        <Wrapper>
+            {props.type === 2 && <Profile size="S" name={props.name}/>}
+            <Container type={props.type}>
+                {props.type === 2 && <p>{props.name}</p>}
+                {props.children}
+                <StatusContainer>
+                    <span>{props.time}</span>
+                    {props.type === 1 && <ReadStatus state={props.readState} />}
+                </StatusContainer>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default MessageContainer;
--- src/app/messenger/messages/chat/MessagesArea.js
+++ src/app/messenger/messages/chat/MessagesArea.js
@@ -1,12 +1,29 @@
 import styled from 'styled-components';
 
+import MessageBubble, { Bubble } from './MessageBubble';
+
 const Container = styled.div`
     flex: 1;
+    padding: 1.2rem;
 `;
 
-function MessagesArea(){
+function MessagesArea() {
     return (
-        <Container/>
+        <Container>
+            <MessageBubble time="12:00" name="Justin Dust" type={2}>
+                <Bubble>Hi</Bubble>
+            </MessageBubble>
+            <MessageBubble time="12:02" name="Alanna Cobbett" type={2}>
+                <Bubble>Hello</Bubble>
+                <Bubble>How are you?</Bubble>
+            </MessageBubble>
+            <MessageBubble time="12:00" name="Justin Dust" type={2}>
+                <Bubble>Fine what about you?</Bubble>
+            </MessageBubble>
+            <MessageBubble type={1} readState={1} time="11:20">
+                <Bubble>Hi Justin, we missed you. <br/> Where were you?</Bubble>
+            </MessageBubble>
+        </Container>
     )
 }
 
